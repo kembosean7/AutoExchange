c7456d69faf1f2f671e12be9ddbc647d2b124220
Sean Kembo <sekembojhb024@student.wethinkcode.co.za>
2025-07-13 10:38:45 +0200

    feat: added method to extract JWT claims and signing key

---
+++ AutoExchange/src/main/java/com/app/AutoExchange/config/JwtService.java
@@ -0,0 +1,43 @@
+package com.app.AutoExchange.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Base64;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+
+    private static String SECRET_KEY =   "404E635266556A586E3272357538782F413F4428472B4B6250645367566B5970";
+
+
+    public String extractUsername(String token) {
+        return null;
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> claimsResolver {
+        return null;
+    }
+
+    private Claims extractAllClaims(String token) {
+        byte[] getSignInKey;
+        return Jwts
+                .parser()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+    }
+
+    private Key getSignInKey(){
+
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
