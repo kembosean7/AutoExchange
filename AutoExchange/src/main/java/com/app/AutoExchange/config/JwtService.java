38420b149b511740523ff83962b601681f081989
Sean Kembo <sekembojhb024@student.wethinkcode.co.za>
2025-07-13 10:59:09 +0200

    feat: add methods to check JWT expiration

--- AutoExchange/src/main/java/com/app/AutoExchange/config/JwtService.java
+++ AutoExchange/src/main/java/com/app/AutoExchange/config/JwtService.java
@@ -83,4 +83,12 @@ public class JwtService {
         final String username = extractUsername(token);
         return (username.equals(userDetails.getUsername())) && !isTokenExpired(token));
     }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
 }
