94d13ce15a357dba598285d555d8d92778f9ff98
Sean Kembo <sekembojhb024@student.wethinkcode.co.za>
2025-07-13 10:01:35 +0200

    Add User Entity with its fields,  Included Lombok annotations for boilerplate reduction

---
+++ AutoExchange/src/main/java/com/app/AutoExchange/user/User.java
@@ -0,0 +1,29 @@
+package com.app.AutoExchange.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String password;
+
+}
