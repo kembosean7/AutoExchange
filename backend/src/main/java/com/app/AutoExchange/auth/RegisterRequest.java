e6bc2f4bee2d7b28673508d86f0931b7aaab4092
Sean Kembo <sekembojhb024@student.wethinkcode.co.za>
2025-07-17 00:35:57 +0200

    feat: add RegisterRequest DTO  with validation rulesannotations for User fields

---
+++ backend/src/main/java/com/app/AutoExchange/auth/RegisterRequest.java
@@ -0,0 +1,40 @@
+package com.app.AutoExchange.auth;
+
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+
+    @NotBlank(message = "First name is required")
+    private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    private String lastName;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email")
+    private String email;
+
+    @NotBlank(message = "Password name is required")
+    @Size(min = 8, message = "Password must be at least 8 characters")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$", message = "Password must contain at least one lowercase letter, one uppercase letter, one digit, and one special character")
+    private String password;
+
+
+    @NotBlank(message = "Confirm password is required")
+    private String confirmPassword;
+
+
+    @AssertTrue(message = "Passwords do not match")
+    public boolean isPasswordMatching() {
+        return password.equals(confirmPassword);
+    }
+}
