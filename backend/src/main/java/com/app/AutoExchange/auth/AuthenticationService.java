14e46b5e808c81692dc3f1981e54ae251749f9af
Sean Kembo <sekembojhb024@student.wethinkcode.co.za>
2025-07-17 00:46:46 +0200

    feat: add AuthenticationService with signup method with check for existing email. Encodes password and builds User entity with User Role

---
+++ backend/src/main/java/com/app/AutoExchange/auth/AuthenticationService.java
@@ -0,0 +1,42 @@
+package com.app.AutoExchange.auth;
+
+import com.app.AutoExchange.config.JwtService;
+import com.app.AutoExchange.user.Role;
+import com.app.AutoExchange.user.User;
+import com.app.AutoExchange.user.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class AuthenticationService {
+
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final UserDetailsService userDetailsService;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse signup(RegisterRequest request) {
+
+        Optional<User> existingUser = repository.findByEmail(request.getEmail());
+        if(existingUser.isPresent()){
+            throw new RuntimeException("User with the same email already exist");
+        }
+
+        var user = User.builder()
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+
+
+    }
+}
