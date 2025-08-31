621588e7567da4a726988975f0084969fc28cef1
ZadenMubarak <zaden.ngobeni@gmail.com>
2025-08-31 14:30:16 +0200

    search bar fixes

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -73,6 +73,7 @@ export default function SearchBar({
           className="absolute inset-y-0 right-0 flex items-center pr-4"
         >
           <div className="relative">
+            
             <svg
               className={`h-5 w-5 ${
                 listening ? "text-blue-600" : "text-gray-400"
@@ -102,7 +103,7 @@ export default function SearchBar({
       {/* Blue search button */}
       <button
         type="submit"
-        className="inline-flex items-center gap-2 rounded-full bg-blue-600 px-4 py-2.5 text-sm font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-500 h-10"
+        className="inline-flex items-center gap-2 rounded-full bg-blue-600 px-4 py-2.5 text-sm font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-4 focus:ring-blue-500 dark:bg-blue-600 dark:hover:bg-blue-500 h-10"
       >
         <svg
           className="h-4 w-4"
