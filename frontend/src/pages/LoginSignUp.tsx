3e0fb6c62df0e20c5985ca27dcfc87dbc42b778d
ZadenMubarak <zaden.ngobeni@gmail.com>
2025-09-01 21:46:23 +0200

    need to add tabs

--- frontend/src/pages/LoginSignUp.tsx
+++ frontend/src/pages/LoginSignUp.tsx
@@ -11,8 +11,8 @@ const LoginSignUp = () => {
         <div className='w-80 mt-20 h-80 shadow-md flex justify-center items-center'>
 
             <div className='flex justify-center items-center gap-1'>
-                <button className='block mx-3 mt-4 bg-brand-blue text-white px-6 py-2 rounded-lg hover:bg-blue-700 transition-colors text-center'>Login</button>
-                <button className='block mx-3 mt-4 bg-brand-blue text-white px-6 py-2 rounded-lg hover:bg-blue-700 transition-colors text-center'>Sign up</button>
+                <button className='block mx-2 mt-4 bg-brand-blue text-white px-6 py-2 rounded-lg hover:bg-blue-700 transition-colors text-center w-auto'>Login</button>
+                <button className='block mx-2 mt-4 bg-brand-blue text-white px-6 py-2 rounded-lg hover:bg-blue-700 transition-colors text-center w-auto'>Sign up</button>
                 {/* <Button>Login</Button> */}
             </div>
         </div>
