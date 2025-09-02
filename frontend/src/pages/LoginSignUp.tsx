ffebc63d5fd098ea11e95cfe65b37190f384bb18
ZadenMubarak <zaden.ngobeni@gmail.com>
2025-09-02 08:00:19 +0200

    adding more components to login

--- frontend/src/pages/LoginSignUp.tsx
+++ frontend/src/pages/LoginSignUp.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
 
 const LoginSignUp = () => {
 
@@ -10,10 +10,18 @@ const LoginSignUp = () => {
         {/* card */}
         <div className='w-80 mt-20 h-80 shadow-md flex justify-center items-center'>
 
+          {/* input */}
+
+          <input className='' /> 
+          <input className='' type="email" name="" id="" />
+          <input className='' type="password" name="" id="" />
+
+
+
+            {/* button login/signup */}
             <div className='flex justify-center items-center gap-1'>
                 <button className='block mx-2 mt-4 bg-brand-blue text-white px-6 py-2 rounded-lg hover:bg-blue-700 transition-colors text-center w-auto'>Login</button>
                 <button className='block mx-2 mt-4 bg-brand-blue text-white px-6 py-2 rounded-lg hover:bg-blue-700 transition-colors text-center w-auto'>Sign up</button>
-                {/* <Button>Login</Button> */}
             </div>
         </div>
     </div>
