3d4499118295d839889456720d750027c51b6657
ZadenMubarak <zaden.ngobeni@gmail.com>
2025-09-05 21:15:17 +0200

    login component (ui part) done now onto sign up, and tabs for login-signup

--- frontend/src/pages/LoginSignUp.tsx
+++ frontend/src/pages/LoginSignUp.tsx
@@ -6,24 +6,46 @@ const LoginSignUp = () => {
 
 
   return (
-    <div className='flex justify-center place-items-center'>
-        {/* card */}
-        <div className='w-80 mt-20 h-80 shadow-md flex justify-center items-center'>
 
-          {/* input */}
-{/* 
-          <input className='' /> 
-          <input className='' type="email" name="" id="" />
-          <input className='' type="password" name="" id="" /> */}
+    <div className='flex justify-center items-center h-screen bg-indigo-600'>
+      <div className='w-96 p-6 shadow-lg bg-white rounded-md'>
+        <h1 className='text-3xl block text-center font-semibold'>Login</h1>
+        <hr className='mt-3 '/>
 
+        <div className='mt-3'>
+          <label htmlFor="username" className='block text-base mb-2'>username</label>
+          <input type="text" id='username'  className='border w-full text-base px-2 py-1 focus:outline-none focus:ring-0 focus:border-gray-600' placeholder='enter username'/>
 
-
-            {/* button login/signup */}
-            <div className='flex justify-center items-center '>
-                <button className='block mx-2 mt-4 bg-brand-blue text-white px-6 py-2 rounded-lg hover:bg-blue-700 transition-colors text-center w-auto'>Login</button>
-                <button className='block mx-2 mt-4 bg-brand-blue text-white px-6 py-2 rounded-lg hover:bg-blue-700 transition-colors text-center w-auto'>Sign up</button>
-            </div>
         </div>
+
+        <div className='mt-3'>
+          <label htmlFor="password" className='block text-base mb-2'>password</label>
+          <input type="passwotd" id='password'  className='border w-full text-base px-2 py-1 focus:outline-none focus:ring-0 focus:border-gray-600' placeholder='enter password'/>
+
+        </div>
+
+
+        <div className='mt-4 flex justify-between items-center'>
+          <div>
+            <input type="checkbox" />
+            <label>Remember me ?</label>
+          </div>
+
+          <div>
+            <a href='#' className='text-indigo-800 font-semibold'>Forgot Password?</a>
+          </div>
+        </div>
+
+        
+
+
+        <div className='mt-5'>
+          <button className='border-2 border-blue-700 bg-blue-600 text-white py-1 px-5 w-full rounded-md hover:bg-transparent hover:text-black font-semibold' type='submit'>Login</button>
+
+        </div>
+
+
+      </div>
     </div>
   )
 }
