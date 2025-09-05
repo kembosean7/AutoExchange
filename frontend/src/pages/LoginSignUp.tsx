a544553ad7109a19c38f659f6ca7582126a2a6a7
ZadenMubarak <zaden.ngobeni@gmail.com>
2025-09-05 15:44:38 +0200

    removed components from login

--- frontend/src/pages/LoginSignUp.tsx
+++ frontend/src/pages/LoginSignUp.tsx
@@ -11,15 +11,15 @@ const LoginSignUp = () => {
         <div className='w-80 mt-20 h-80 shadow-md flex justify-center items-center'>
 
           {/* input */}
-
+{/* 
           <input className='' /> 
           <input className='' type="email" name="" id="" />
-          <input className='' type="password" name="" id="" />
+          <input className='' type="password" name="" id="" /> */}
 
 
 
             {/* button login/signup */}
-            <div className='flex justify-center items-center gap-1'>
+            <div className='flex justify-center items-center '>
                 <button className='block mx-2 mt-4 bg-brand-blue text-white px-6 py-2 rounded-lg hover:bg-blue-700 transition-colors text-center w-auto'>Login</button>
                 <button className='block mx-2 mt-4 bg-brand-blue text-white px-6 py-2 rounded-lg hover:bg-blue-700 transition-colors text-center w-auto'>Sign up</button>
             </div>
