1e42898f81be4db6ec26ec249915b34fe60d73bd
ZadenMubarak <zaden.ngobeni@gmail.com>
2025-09-15 22:24:29 +0200

    fix pipeline

--- frontend/src/pages/Inventory.tsx
+++ frontend/src/pages/Inventory.tsx
@@ -1,8 +1,27 @@
 import React from 'react'
 
+import { useNavigate } from 'react-router-dom'
+
+
+/**
+ * 
+ * @returns Inventory Page
+ * load cars from backend
+ * display them in the inventory page
+ * each car should be shown as a card
+ * clicking on "view more" should navigate to the car details page
+ */
+
 const Inventory = () => {
+  const navigate = useNavigate()
+
+
+
   return (
-    <div>Inventory</div>
+    <div>
+      <h1>Inventory</h1>
+      
+    </div>
   )
 }
 
