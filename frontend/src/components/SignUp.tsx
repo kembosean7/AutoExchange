76f1954182ed34396d1f7c1d2ff36baa0a26dca7
Sean Kembo <sekembojhb024@student.wethinkcode.co.za>
2025-09-30 17:36:18 +0200

    fix: update sign up url

--- frontend/src/components/SignUp.tsx
+++ frontend/src/components/SignUp.tsx
@@ -26,7 +26,7 @@ const SignUp = () => {
       return;
     }
     try {
-      const res = await fetch('api/signup', {
+      const res = await fetch('http://localhost:8080/api/v1/auth/signup', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(form)
