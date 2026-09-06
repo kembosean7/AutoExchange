e1bb00f59615928e04c40a9e0484cf73ea85bc04
Sean Kembo <kembosean7@gmail.com>
2026-09-06 09:20:06 +0200

    Allow origin for git codespace

--- backend/src/main/java/com/app/AutoExchange/config/CorsConfig.java
+++ backend/src/main/java/com/app/AutoExchange/config/CorsConfig.java
@@ -13,7 +13,8 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("http://localhost:5173",
+                                        "https://glowing-computing-machine-9rvgx6prgprfx579-5173.app.github.dev")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
